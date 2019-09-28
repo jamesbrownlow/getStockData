2a80db70bad03a45c9224248c000ccfd502dd710
Dr. J <jbrownlow@avc.edu>
2019-09-27 20:18:23 -0400

    Add files via upload

--- getStockDataWeb.R
+++ getStockDataWeb.R
@@ -6,19 +6,15 @@ library(xts)
 library(BatchGetSymbols)
 
 # set dates
-
-first.date = "2018-04-01"
-#last.date <- Sys.Date()
+first.date <- Sys.Date() - 550
+last.date <- Sys.Date()
 freq.data <- 'daily'
 # set tickers
-#tickers <- c('NFG','AAPL' )
-
-stockName=c("AAPL","HD","SLP")
-tickers = stockName
+tickers <- c('NFLX','HD' )
 
 l.out <- BatchGetSymbols(tickers = tickers, 
                          first.date = first.date,
-#                         last.date = last.date, 
+                         last.date = last.date, 
                          freq.data = freq.data,
                          cache.folder = file.path(tempdir(), 
                         'BGS_Cache') ) # cache in tempdir()
@@ -27,20 +23,24 @@ head(l.out)
 
 
 ## stock names:
-
-
-for(i in 1:3) {
-
-plot(l.out$df.tickers$volume[l.out$df.tickers$ticker == stockName[i]],
-     type='l', ylab = 'volume', xlab = 'day', main=stockName[i])
+plot(l.out$df.tickers$volume[l.out$df.tickers$ticker == tickers[1]]/1.e5,type='l',
+     ylab = 'volume (10000s)', xlab = 'day', main=tickers[1])
 grid()
 
-plot(l.out$df.tickers$price.close[l.out$df.tickers$ticker == stockName[i]],
-     type='l', ylab = 'price $', xlab = 'day', main=stockName[i])
+plot(l.out$df.tickers$price.close[l.out$df.tickers$ticker == tickers[1]],
+     type='l', ylab = 'price $', xlab = 'day', main=tickers[1])
 grid()
-}
 
-write.csv(l.out$df.tickers[l.out$df.tickers$ticker == 'SLP',],'SLP.csv')
 
-plot(l.out$df.tickers$ref.date,l.out$df.tickers$price.close,type='l')
+plot(l.out$df.tickers$volume[l.out$df.tickers$ticker == tickers[2]]/1.e5,type='l',
+     ylab = 'volume (10000s)', xlab = 'day', main=tickers[2])
+grid()
+
+plot(l.out$df.tickers$price.close[l.out$df.tickers$ticker == tickers[2]],
+     type='l', ylab = 'price $', xlab = 'day', main=tickers[2])
+grid()
+
+
+#write.csv(l.out$df.tickers,'NFLX.csv')
+
 
